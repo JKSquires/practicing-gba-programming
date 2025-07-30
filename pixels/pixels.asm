0a31b8434fa2a23c1690b255b41d8ac71b492fbe
Jason Squires <168765098+JKSquires@users.noreply.github.com>
2025-07-30 18:04:02 -0500

    Upload pixels source

---
+++ pixels/pixels.asm
@@ -0,0 +1,28 @@
+b start
+
+start:
+
+mov r0,0x04000000
+ldr r1,=0x0403
+strh r1,[r0]
+
+ldr r1,=0x7FE0
+ldr r2,=0x01DE ; 239px * 2
+
+mov r0,0x06000000
+strh r1,[r0]
+
+mov r1,r1,lsr 5 ; note: lsr and lsl seems to be unsupported. The current line still gets assembled into the correct 0xE1A012A1 for lsr r1,r1,5
+add r0,r0,r2
+strh r1,[r0]
+
+mov r1,r1,lsr 5
+ldr r0,=0x06012A20
+strh r1,[r0]
+
+mov r1,r1,lsl 5
+add r0,r0,r2
+strh r1,[r0]
+
+loop:
+b loop
