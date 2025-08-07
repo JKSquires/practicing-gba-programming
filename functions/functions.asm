d0a86b6d916733b8f0a21e42c3e8cd932d1f1fa6
Jason Squires <168765098+JKSquires@users.noreply.github.com>
2025-08-07 15:26:03 -0500

    Add functions.asm

---
+++ functions/functions.asm
@@ -0,0 +1,32 @@
+b start
+
+@include header.asm
+
+drawPixel: ; r0: x-coord; r1: y-coord; r2: color. Uses registers r3-r4
+mov r3,0xF0
+mul r4,r1,r3
+add r4,r4,r0
+mov r4,r4,lsl 1
+
+orr r3,r4,0x6000000
+strh r2,[r3]
+
+mov r15,r14
+
+start:
+mov r0,0x4000000
+ldr r1,=0x403
+strh r1,[r0]
+
+mov r0,10
+mov r1,20
+mov r2,0xFF
+bl drawPixel
+
+mov r0,20
+mov r1,10
+mov r2,0xFF0
+bl drawPixel
+
+loop:
+b loop
